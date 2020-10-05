de268db50037cecde3fc4ccb7f1553b1248d484e
Anu Rav <anusharavva@gmail.com>
2020-10-04 18:49:35 -0700

    Added the functionality to all the Save button events

--- Assets/script.js
+++ Assets/script.js
@@ -22,16 +22,21 @@ for(var i = 0; i < hours.length ;i++)
     let col1Text = $("<span>");
      //string manipulation
      let str = hours[i];
+     let newAttrVal = str+"block";
      str = str.substring(0, str.length - 2);
     col1Text.text(hours[i]);
     col2.attr("id",str);
+    col2.attr("blockName",newAttrVal);
     mainContainer.append(newSection);
     newSection.append(col1);
     col1.append(col1Text);
     newSection.append(col2);
     newSection.append(col3);
     col3.append(icon);
+    col3.attr("blockName",newAttrVal);
     textInputColor(str,col2);
+    let userData = localStorage.getItem(newAttrVal) || "";
+    col2.val(userData);
 }
 
 //Setting Text Area backgound color 
@@ -51,11 +56,25 @@ function textInputColor(str,col2)
     }
     else if (textareaHour < currentHour)
     {
-        col2.attr("style","background-color:blue");
+        col2.attr("style","background-color:white");
     }
     else {
         col2.attr("style","background-color:red");
     }
 }
+
+//Click event for save Button
+
+$(".saveBtn").click(function(){
+
+let attrVal = $(this).attr('blockName');
+var id = $(this).parents().children("textarea").val();
+localStorage.setItem(attrVal,id);
+
+})
+
+
+
+
 
 

43fe91a6d49ae7840fb3c82abc29182392f2a0d8
Anu Rav <anusharavva@gmail.com>
2020-10-04 19:20:16 -0700

    Added comments and organized the code into understandable blocks

--- Assets/script.js
+++ Assets/script.js
@@ -2,44 +2,56 @@
 
 $("#currentDay").text(moment().format('dddd [,] MMMM Do'));
 
-//Defining working hours in an array
+//variables declaration
 
 let hours = ["9AM", "10AM", "11AM", "12PM","1PM","2PM","3PM","4PM","5PM"];
+let mainContainer = $(".container");
 
+//for loop to create block for each working hour
 for(var i = 0; i < hours.length ;i++)
 {
-    let mainContainer = $(".container");
-    let newSection = $("<div>");
+    let str = hours[i];
+    let newAttrVal = str+"block"; 
+    let userData = localStorage.getItem(newAttrVal) || "";
+
+     //string manipulation to extract the number and slice AM/PM from the time
+    newStr = str.substring(0, str.length - 2);
+
+    //Creating a parent section for each hour block and assigning attributes
+    let newSection = $("<section>");
     newSection.attr("class","row time-block");
+    mainContainer.append(newSection);
+
+    //Creating child elements and appending it to the parent section
+    //child 0 and its attributes declaration
     let col1 = $("<div>");
     col1.attr("class","col-1 hour");
+    newSection.append(col1);
+    let col1Text = $("<span>");
+    col1Text.text(hours[i]);
+    col1.append(col1Text);
+
+    //child 1 and its attributes declaration
     let col2 = $("<textarea>");
     col2.attr("class","col-10 description");
-    let col3 = $("<div>");
-    col3.attr("class","col-1 saveBtn");
-    let icon = $("<i>");
-    icon.attr("class","fas fa-save");
-    let col1Text = $("<span>");
-     //string manipulation
-     let str = hours[i];
-     let newAttrVal = str+"block";
-     str = str.substring(0, str.length - 2);
-    col1Text.text(hours[i]);
     col2.attr("id",str);
     col2.attr("blockName",newAttrVal);
-    mainContainer.append(newSection);
-    newSection.append(col1);
-    col1.append(col1Text);
     newSection.append(col2);
-    newSection.append(col3);
-    col3.append(icon);
-    col3.attr("blockName",newAttrVal);
-    textInputColor(str,col2);
-    let userData = localStorage.getItem(newAttrVal) || "";
     col2.val(userData);
+    //function call to determine the background color of text area
+    textInputColor(newStr,col2);
+
+    //child 2 and its attributes declaration
+    let col3 = $("<div>");
+    col3.attr("class","col-1 saveBtn");
+    col3.attr("blockName",newAttrVal);
+    let icon = $("<i>");
+    icon.attr("class","fas fa-save");
+    newSection.append(col3);
+    col3.append(icon); 
 }
 
-//Setting Text Area backgound color 
+//Function to set the background color of each text area 
 
 function textInputColor(str,col2)
 {
